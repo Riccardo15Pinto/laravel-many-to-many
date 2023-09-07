768ff4cc1c10231fb6ccb151d62e6d300165846b
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-07 16:22:15 +0200

    set technology route

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Technology;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TechnologyController extends Controller
 {
@@ -12,7 +14,9 @@ class TechnologyController extends Controller
      */
     public function index()
     {
-        //
+
+        $techs = Technology::all();
+        return view('admin.technology.index', compact('techs'));
     }
 
     /**
@@ -20,7 +24,8 @@ class TechnologyController extends Controller
      */
     public function create()
     {
-        //
+        $techs = new Technology();
+        return view('admin.technology.create', compact('techs'));
     }
 
     /**
@@ -28,38 +33,73 @@ class TechnologyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data_new_tech = $request->all();
+        $request->validate(
+            [
+                'label' => 'required|string|max:30',
+                'color' => 'nullable|regex:/^#(?:[0-9a-fA-F]{3}){1,2}$/',
+                'info' => 'nullable|string',
+            ],
+            [
+                'label.required' => 'Il nome della tipologia è obbligatorio',
+                'label.max' => 'Il nome della tipologia è troppo lungo',
+                'color.regex' => 'Il colore inserito non è un colore esadecimale valido',
+            ]
+        );
+
+        $tech = new Technology();
+        $tech->fill($data_new_tech);
+        $tech->save();
+
+        return to_route('admin.technology.index')->with('alert-tech', 'success')->with('alert-message', "$tech->label creato con successo");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Technology $tech)
     {
-        //
+        return view('admin.technology.show', compact('tech'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Technology $tech)
     {
-        //
+        return view('admin.technology.edit', compact('type'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Technology $tech)
     {
-        //
+        $data_new_tech = $request->all();
+        $request->validate(
+            [
+                'label' => ['required', 'string', 'max:30', Rule::unique('technologies')->ignore($tech)],
+                'color' => 'nullable|regex:/^#(?:[0-9a-fA-F]{3}){1,2}$/',
+            ],
+            [
+                'label.required' => 'Il nome della tipologia è obbligatorio',
+                'label.max' => 'Il nome della tipologia è troppo lungo',
+                'color.regex' => 'Il colore inserito non è un colore esadecimale valido',
+            ]
+        );
+
+        $$tech->update($data_new_tech);
+        return to_route('admin.technology.show', compact('type'))->with('alert-type', 'success')->with('alert-message', "$tech->label modificato con successo");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Technology $tech)
     {
-        //
+        $tech->delete();
+        return to_route('admin.technology.index')
+            ->with('alert-type', 'success')
+            ->with('alert-message', "$tech->label eliminato con successo"); //
     }
 }
